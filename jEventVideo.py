5c519c3c71087db70c79a1a432e1a60b2fba9707
API Geoje <apigeoje@yahoo.com>
2016-02-05 15:41:52 +0900

    Remove jEventVideo

--- jEventVideo.py
+++
@@ -1,3 +0,0 @@
-import logging
-logging.basicConfig(filename='/var/log/jBabyMonitor.log', format='%(asctime)s %(filename)s %(message)s', datefmt='%y-%m-%d %H:%M', level=logging.DEBUG)
-logging.info("jEventVideo.py initialising")
