afa9f58cd2dc01df8977a63c7f6e914edf463e13
API Geoje <apigeoje@yahoo.com>
2016-02-05 15:34:00 +0900

    jVideoCapture and jYouTubeUploader

---
+++ jVideoCapture.py
@@ -0,0 +1,276 @@
+#!/usr/bin/python
+'''-------------------------------------
+
+  Dependencies:
+  -------------
+  io
+  picamera
+  RPi.GPIO
+  threading
+  # pyaudio
+
+
+  This app will do the following
+
+  1:  get the camera
+      create a circular buffer
+      start recording into the circular buffer
+
+  2:  initiate the sensor
+      check that the sensor is ready
+      set the sensor
+
+  3:  enter a loop
+      wait for a Rising/Falling edge
+
+  4:  when the sensor detects movement
+      split recording to after file
+      write the circular buffer to a file named <timestamp>
+      clear the buffer
+      wait for a the sensor to reset
+
+  5:  when reset split back to circular
+      write the after file
+
+  6:  concat the two files into 1
+
+  7:  http put the event to node-red
+
+  8:  loop
+
+----------------------------------------
+  Dependencies
+------------------------------------------'''
+import io
+import os
+import picamera
+import requests
+import RPi.GPIO as GPIO
+import socket
+import threading
+import time
+import subprocess as sp
+
+'''-----------------------------------------
+  Global configuration variables
+------------------------------------------'''
+# for logging
+APP_NAME = 'jBabyMonitor'
+# where streams will be saved before concatenation
+TEMP_STORAGE_PATH = ''
+# where concatenated files will be stored
+OUTPUT_STORAGE_PATH = '/home/pi/Videos/'
+# resolution
+CAMERA_RESOLUTION = (1296,730)
+# bitrate
+CAMERA_BITRATE = 1000000
+# length of circular buffer and time recorded before a triggered event
+BUFFER_SECS = 5
+# the pin number
+SENSOR_CHANNEL = 21
+# camera splitters
+MONITOR_SPLITTER = 1
+STREAM_SPLITTER = 2
+# ffmpeg
+FFMPEG_BIN = "ffmpeg"
+# youtubeupload script
+YOUTUBE_UPLOADER = "./jYouTubeUploader.py"
+# how big a movement stream is significant
+SIGNIFICANT_STREAM_SIZE = 2500000
+
+'''-----------------------------------------
+  Helper functions
+------------------------------------------'''
+
+def write_video(stream, filename):
+    # Write the entire content of the circular buffer to disk. No need to
+    # lock the stream here as we're definitely not writing to it
+    # simultaneously
+
+    with io.open(TEMP_STORAGE_PATH + filename, 'wb') as output:
+        if isinstance(stream, picamera.streams.PiCameraCircularIO):
+            for frame in stream.frames:
+                if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                    stream.seek(frame.position)
+                    break
+            while True:
+                buf = stream.read1()
+                if not buf:
+                    break
+                output.write(buf)
+            retFileSize = 0
+
+        elif isinstance(stream, io.BytesIO):
+            retFileSize = stream.tell()
+            stream.seek(0)
+            buf = stream.getvalue()
+            output.write(buf)
+
+        else:
+            raise TypeError('Expecting a PiCameraCircularIO or an io.BytesIO')
+
+        # clear the stream and prepare it for reuse
+        stream.seek(0)
+        stream.truncate()
+        return retFileSize, filename
+
+def concat(clip, filename):
+
+    try:
+        concatString = 'concat:'+clip[0]+'|'+clip[1]
+
+        command = [ FFMPEG_BIN,
+           '-y', # (optional) overwrite output file if it exists
+           '-i', concatString,
+           '-c', 'copy',
+           TEMP_STORAGE_PATH+filename ]
+
+        pipe = sp.Popen( command, stdin=sp.PIPE, stderr=sp.PIPE)
+
+        pipe.wait()
+        return filename 
+    except:
+        return False
+
+
+def removeFiles(fileArray):
+    for file in fileArray:
+	os.remove(file)
+
+def pushToYouTube(outputFile):
+
+    try:
+        command = [ YOUTUBE_UPLOADER,
+            '--file', TEMP_STORAGE_PATH+outputFile,
+            '--title', outputFile,
+            '--noauth_local_webserver'
+           ]
+
+        youtubeId = sp.check_output( command ) 
+
+	return youtubeId.strip()
+
+    except:
+        return False
+
+def logToNodeRed(eventObj):
+    # requests to node-red
+    r = requests.post('http://localhost:1880/video', json=eventObj)
+    if r.status_code != 200:
+        print("Problem :/\n")
+
+'''------------------------------------------
+  Classes
+------------------------------------------'''
+
+class jBabyMonitor(threading.Thread):
+
+    def __init__(self, camera):
+        threading.Thread.__init__(self)
+        self.camera = camera
+
+    def run(self):
+        jEvent = {}
+        time.sleep(1800)
+        # create the circular buffer
+        stream = picamera.PiCameraCircularIO(camera,
+                                             seconds=BUFFER_SECS,
+                                             bitrate=CAMERA_BITRATE,
+                                             splitter_port=MONITOR_SPLITTER)
+
+        # write a linear stream to store the after
+        after = io.BytesIO()
+        # start recording to the circular buffer
+        self.camera.start_recording(stream,
+                                    format='h264',
+                                    bitrate=CAMERA_BITRATE,
+                                    splitter_port=MONITOR_SPLITTER)
+
+
+        # set up the IR sensor
+        GPIO.setup(SENSOR_CHANNEL, GPIO.IN)
+        self.camera.wait_recording(BUFFER_SECS, splitter_port=MONITOR_SPLITTER)
+        try:
+            # enter the loop
+            while camera:
+                if not GPIO.input(SENSOR_CHANNEL):
+                    GPIO.wait_for_edge(SENSOR_CHANNEL, GPIO.RISING)
+
+                # set the timestamp
+                jEvent['timestamp'] = int(time.time()*1000)
+                jEvent['datetime'] = time.strftime('%Y-%m-%d %H:%M') 
+                # As soon as we detect motion, split the recording to
+                # record the frames "after" motion
+                self.camera.split_recording(after,splitter_port=MONITOR_SPLITTER)
+
+                # build the output filename
+                outputFile = str(int(time.time()))
+
+                # Write the "before" motion to disk as well
+                beforeFileSize, beforeFileName = write_video(stream, outputFile+'.h264')
+
+                # print '# Wait until motion is no longer detected'
+                GPIO.wait_for_edge(SENSOR_CHANNEL, GPIO.FALLING)
+
+                # calculate the activity duration
+                jEvent['duration'] = int((int(time.time()*1000) - jEvent['timestamp'])/1000)
+
+                # then split recording back to the in-memory circular buffer
+                self.camera.split_recording(stream,splitter_port=MONITOR_SPLITTER)
+
+                # Write the "after" motion to disk
+                afterFileSize, afterFileName = write_video(after, 'after'+outputFile+'.h264')
+
+                if afterFileSize > 2500000:
+                    jEvent['filename'] = outputFile + '.mpg'
+                    ## concatenate two file
+                    concatSuccess = concat([beforeFileName, afterFileName], jEvent['filename'])
+        	    if concatSuccess:
+                        jEvent['YTid'] = pushToYouTube(jEvent['filename'])
+                        if jEvent['YTid']:
+                            removeFiles([beforeFileName, afterFileName, jEvent['filename']])
+                    else:
+                        print 'Problem writing the compiled file,' + outputFile + '... do it manually later'
+                else:
+	            removeFiles([beforeFileName, afterFileName]) 
+
+                jEvent['source'] = APP_NAME 
+                jEvent['activity'] = 'Movement'
+                jEvent['mood'] = 'Active Baby!'
+                jEvent['location'] = 'Cot'
+                # the sensor takes about 10 seconds to turn off so 
+                # if the duration is less than 10 it is a hangover 
+                # from the last event or too short to bother
+                if jEvent['duration'] > 10:
+		    logToNodeRed(jEvent)
+                jEvent = {}
+
+        finally:
+            self.camera.stop_recording()
+            GPIO.cleanup()
+
+
+class jBabyWebstream(threading.Thread):
+    def __init__(self, camera):
+        print 'jBabyWebstream has initiated'
+        threading.Thread.__init__(self)
+        self.camera = camera
+
+    def run(self):
+        print 'Started the Webstream... :p yeah right!!!!!'
+
+
+GPIO.setmode(GPIO.BCM)
+
+'''-----------------------------------------
+  Setup the camera
+------------------------------------------'''
+with picamera.PiCamera() as camera:
+    camera.resolution = CAMERA_RESOLUTION
+    camera.hflip = True
+    camera.vflip = True
+
+    myBabyMon = jBabyMonitor(camera)
+    myBabyMon.start()
+    raw_input("Hit enter to close the Babymonitor")
+    print 'Good-bye!'
---
+++ jYouTubeUploader.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+
+import httplib
+import httplib2
+import os
+import random
+import requests
+import sys
+import threading
+import time
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+from string import Template
+
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+  httplib.IncompleteRead, httplib.ImproperConnectionState,
+  httplib.CannotSendRequest, httplib.CannotSendHeader,
+  httplib.ResponseNotReady, httplib.BadStatusLine)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+CLIENT_SECRETS_FILE = "/home/jeremy/jPyPi/client_secrets.json"
+MISSING_CLIENT_SECRETS_MESSAGE = "Missing client secrets message"
+# This OAuth 2.0 access scope allows an application to upload files to the
+# authenticated user's YouTube channel, but doesn't allow other types of access.
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+
+YOUTUBE_PRIVACY_STATUS = 'unlisted' # can also be 'public' or 'unlisted'
+YOUTUBE_CATEGORY_ID = '22' # video
+YOUTUBE_DESCRIPTION_TEMPLATE = "This is a video of $who on $when. \n\n\
+            It was filmed $where by $filmer."
+
+JBABYMON_TAG = ['movement']
+msg = {}
+jEvent = {}
+
+class jYouTubeUploader(threading.Thread):
+    '''
+        This class provides a simple interface to upload files
+        to youtube.
+
+        It is intended to work on the raspberry pi in conjunction
+        with a video recording process.
+
+        It receives the filename which should be any acceptable
+        command line path i.e., file, ./file, /full/path/to/file
+
+        Also it receives metadata for posting with the video.
+        The format of this data is determined by the
+        DESCRIPTION_TEMPLATE above. every $ variable must have a
+        corresponding element in the metadata object
+    '''
+
+    def __init__(self, filename, metadata, args):
+        threading.Thread.__init__(self)
+        if not filename and not args.file:
+            raise ValueError("Please specify a valid filename")
+	elif not args.file:
+	    self.filename = filename
+        else: 
+            self.filename = args.file
+
+        if args.title:
+            self.title = args.title
+        else:
+            title = self.filename.split('/')
+            self.title = title[len(title)-1]
+
+        self.body = dict(
+                        snippet=dict(
+                            title=self.title,
+                            description=Template(YOUTUBE_DESCRIPTION_TEMPLATE).substitute(metadata),
+                            tags=JBABYMON_TAG,
+                            categoryId=YOUTUBE_CATEGORY_ID
+                        ),
+                        status=dict(
+                            privacyStatus=YOUTUBE_PRIVACY_STATUS
+                        )
+                    )
+
+        self.youtube = self.get_authenticated_service(args)
+        
+
+    def run(self):
+
+        try:
+        # Call the API's videos.insert method to create and upload the video.
+            self.insert_request = self.youtube.videos().insert(
+              part=",".join(self.body.keys()),
+              body=self.body,
+            # The chunksize parameter specifies the size of each chunk of data, in
+            # bytes, that will be uploaded at a time. Set a higher value for
+            # reliable connections as fewer chunks lead to faster uploads. Set a lower
+            # value for better recovery on less reliable connections.
+            #
+            # Setting "chunksize" equal to -1 in the code below means that the entire
+            # file will be uploaded in a single HTTP request. (If the upload fails,
+            # it will still be retried where it left off.) This is usually a best
+            # practice, but if you're using Python older than 2.6 or if you're
+            # running on App Engine, you should set the chunksize to something like
+            # 1024 * 1024 (1 megabyte).
+              media_body=MediaFileUpload(self.filename, chunksize=-1, resumable=True)
+            )
+
+            self.resumable_upload(self.insert_request)
+
+        except HttpError, e:
+            msg['error'] = e
+            print msg['error']
+
+
+
+    def get_authenticated_service(self, args):
+
+        self.flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+          message=MISSING_CLIENT_SECRETS_MESSAGE,
+          scope=YOUTUBE_UPLOAD_SCOPE)
+
+        self.storage = Storage("%s-oauth2.json" % sys.argv[0])
+        self.credentials = self.storage.get()
+
+        if self.credentials is None or self.credentials.invalid:
+          flags = argparser.parse_args()
+          self.credentials = run_flow(self.flow, self.storage, flags)
+
+        return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+          http=self.credentials.authorize(httplib2.Http()))
+
+
+    # This method implements an exponential backoff strategy to resume a
+    # failed upload.
+    def resumable_upload(self, insert_request):
+        response = None
+        error = None
+        retry = 0
+        while response is None:
+            try:
+                #print "Uploading file..."
+                status, response = insert_request.next_chunk()
+                if response is not None:
+                    if 'id' in response:
+                        print response['id']
+                    else:
+                        sys.exit("The upload failed with an unexpected response: %s" % response)
+            except HttpError, e:
+                if e.resp.status in RETRIABLE_STATUS_CODES:
+                    error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                                 e.content)
+                else:
+                    raise
+            except RETRIABLE_EXCEPTIONS, e:
+                error = "A retriable error occurred: %s" % e
+
+        if error is not None:
+            retry += 1
+            if retry > MAX_RETRIES:
+                sys.exit("No longer attempting to retry.")
+
+            max_sleep = 2 ** retry
+            sleep_seconds = random.random() * max_sleep
+            #"Sleeping %f seconds and then retrying..." % sleep_seconds
+            time.sleep(sleep_seconds)
+    
+
+if __name__ == '__main__':
+    argparser.add_argument("--file", required=True, help="Video file to upload")
+    argparser.add_argument("--title", help="Video title", default=None)
+    argparser.add_argument("--description", help="Video description",
+                 default="Test Description")
+    argparser.add_argument("--category", default="22",
+                 help="Numeric video category. " +
+                 "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
+    argparser.add_argument("--keywords", help="Video keywords, comma separated",
+                 default="")
+    argparser.add_argument("--privacyStatus", 
+                 default=YOUTUBE_PRIVACY_STATUS, help="Video privacy status.")
+    args = argparser.parse_args()
+    meta = dict(who="Bump",when=int(time.time()), where="here",filmer="Jeremy")
+
+    upl = jYouTubeUploader(None, meta, args)
+    upl.start()
+    upl.join()
